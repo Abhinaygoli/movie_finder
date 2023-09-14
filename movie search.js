057350fbc7ec4a868b2b06a7be792bf02b9db9f2
Abhinay Goli <goliabhinay5719@gmail.com>
2023-09-14 11:14:58 +0530

    adding movie name and year

--- movie search.js
+++ movie search.js
@@ -1,9 +1,3 @@
-
-/*
-http://www.omdbapi.com/?i=tt3896198&apikey=8ad164ad&s=";
-"http://www.omdbapi.com/?apikey=8ad164ad&i= */
-
-
 var searchButtonElement = document.getElementById("search")
 var inputTagElement = document.getElementById('input-tag')
 var movieWrapperElement = document.getElementById("movie-wrapper")
@@ -15,31 +9,22 @@ searchButtonElement.addEventListener("click",function(){
     if(valueInInputTag == ""){
         alert ("Please enter any movie name")
     }else{
-        $.get("http://www.omdbapi.com/?i=tt3896198&apikey=8ad164ad&s="+valueInInputTag , function(reponse){
+        $.get("https://www.omdbapi.com/?apikey=45f0782a&s="+valueInInputTag , function(reponse){
             loadingMessageElement.innerText = "";
             var returnedArray = reponse
             var movieNamesArray = reponse.Search
-            console.log(movieNamesArray)
             movieWrapperElement.innerHTML = ""
             inputTagElement.value = ""
             $("#message").css({"display":"none"})
             if(returnedArray.Response == "True"){
                 for(var i = 0 ;i < movieNamesArray.length; i++){
-                    console.log(movieNamesArray[i].imdbID)
-                    $.get("http://www.omdbapi.com/?apikey=8ad164ad&i= "+movieNamesArray[i].imdbID,function(reponse){
-                    var movieimdbIDList = reponse    
-                    console.log(movieimdbIDList)
-                        movieWrapperElement.innerHTML += `
+                    movieWrapperElement.innerHTML += `
                         <div class="movie-card">
-                            <img src="${movieimdbIDList.Poster}" class="movie-poster"/>
-                            <p class="movie-title">Movie :${movieimdbIDList.Title}</p>
-                            <p class="movie-year">Director :${movieimdbIDList.Director}</p>
-                            <p class="movie-year">Genre :${movieimdbIDList.Genre}</p>
-                            <p class="movie-year">Year :${movieimdbIDList.Year}</p>
-                            <p class="movie-year">Country :${movieimdbIDList.Country}</p>
+                            <img src="${movieNamesArray[i].Poster}" class="movie-poster"/>
+                            <p class="movie-title">Movie :${movieNamesArray[i].Title}</p>
+                            <p class="movie-year">Year :${movieNamesArray[i].Year}</p>
                         </div>
                         `
-                    })
                 }   
             }else{
                 $("#message").css({"display":"flex"})
